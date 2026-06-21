8c0a55620e37e7e329e69c7a7510eff0aa090f1b
anmol53mahajan <anmolmahajan3507@gmail.com>
2026-06-21 17:17:18 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
         Scanner sc=new Scanner(System.in);
-        String s=sc.next();
-        System.out.println(s+": command not found");
+        while (true) { 
+            System.out.print("$ ");
+            String s=sc.next();
+            System.out.println(s+": command not found");
+        }
     }
 }
